2c5a702a457b5bd296015e18c4cf9fed38cb55ae
brutal-log <gmorales@netwarmonitor.com>
2015-11-05 15:15:59 -0600

    dummy

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gmorales
+ * Date: 11/5/15
+ * Time: 9:57 AM
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>:: CONTINENTAL :: SCRAP ::</title>
+    <link rel="stylesheet" type="text/css" href="css/scrap.css">
+    <script src="js/jquery-1.11.3.min.js"></script>
+</head>
+<body>
+<div id="divHeader">
+    SCRAP
+</div>
+<div id="divMenubar">
+    Bienvenido - Welcome - Willkommen
+</div>
+<div id="divWorkarea">
+    <div id="divLogin">
+        <label class=" label ">Gafete</label>
+        <input class=" text" type="text" value="735443" />
+        <input class=" button " type="button" value="capturar" />
+        <div id="divSeparator"></div>
+        <label class=" label ">Usuario</label>
+        <input class=" text " type="text" value="" />
+        <br />
+        <label class=" label ">Contraseña</label>
+        <input class=" text " type="password" value="" />
+        <input class=" button " type="button" value="login" />
+    </div>
+</div>
+</body>
+</html>
